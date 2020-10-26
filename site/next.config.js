c9c3182c39fc5ab07d015f71336e0269036dce45
Mario Souto <soutomarios@gmail.com>
2020-10-26 18:23:44 -0300

    Add support to fix of the FAQ and introduce the styled components bug

--- site/next.config.js
+++ site/next.config.js
@@ -16,8 +16,8 @@ module.exports = withPlugins(
        * "Invalid hook call"
        * https://github.com/martpie/next-transpile-modules#i-have-trouble-with-duplicated-dependencies-or-the-invalid-hook-call-error-in-react
        * */ 
-      // config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
-      // config.resolve.alias['styled-components'] = path.resolve(__dirname, '.', 'node_modules', 'styled-components');
+      config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
+      config.resolve.alias['styled-components'] = path.resolve(__dirname, '.', 'node_modules', 'styled-components');
       return config
     },
   })],
