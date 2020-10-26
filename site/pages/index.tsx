cf8863c8fad3185e95797f1b15baecda03da7289
Mario Souto <soutomarios@gmail.com>
2020-10-26 17:55:48 -0300

    Working without storybook

---
+++ site/pages/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from '@org/components/src/Button'; 
+
+const Section = styled.section`
+  background: red;
+  color: white;
+`;
+
+
+const Example: string = 'NextJS';
+
+function HomePage() {
+  return (
+    <div>
+      Check view-source to check if the relative CSS is being rendered
+      <Section>
+        background: red;
+        color: white;
+      </Section>
+      <Button type="submit">
+        color: purple;
+      </Button>
+    </div>
+  );
+}
+
+export default HomePage;
